2c00a4d378c6bdebc7e2a44f64c7e0f4124f2a9b
Algernon132 <40308777+Algernon132@users.noreply.github.com>
2018-07-06 18:20:28 -0500

    Added dog class

--- dog/Dog/Dog/Program.cs
+++ dog/Dog/Dog/Program.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Dog
 {
+    class Dog
+    {
+        public string name;
+        public string owner;
+        public int age;
+        public Gender gender;
+    }
     class Program
     {
+        enum Gender { Male, Female };
         static void Main(string[] args)
         {
         }
