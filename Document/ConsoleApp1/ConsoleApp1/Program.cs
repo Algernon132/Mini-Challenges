c7215b3d28068c55851bb5ad6ad6abef4f58a89b
Algernon132 <40308777+Algernon132@users.noreply.github.com>
2018-07-06 19:55:31 -0500

    Added GoAgain()

--- Document/ConsoleApp1/ConsoleApp1/Program.cs
+++ Document/ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,33 +12,60 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            bool again = true;
             Console.WriteLine("-----  Document  -----\n");
-            Console.WriteLine("Enter the name of the document you would like to create.");
 
-            string docName = Console.ReadLine();
-            docName += ".txt";  //add .txt to end of filename
-
-            Console.WriteLine("Enter the content of the new document.");
-
-            string docContent = Console.ReadLine();
-
-            try
+            do
             {
-                StreamWriter sw = new StreamWriter(docName);
+                Console.WriteLine("Enter the name of the document you would like to create.");
 
-                sw.WriteLine(docContent);
+                string docName = Console.ReadLine();
+                docName += ".txt";  //add .txt to end of filename
 
-                sw.Close();
-            }
-            catch(Exception e)
+                Console.WriteLine("Enter the content of the new document.");
+
+                string docContent = Console.ReadLine();
+
+                try
+                {
+                    StreamWriter sw = new StreamWriter(docName);
+
+                    sw.WriteLine(docContent);
+
+                    sw.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);   //display exceptions
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
+                }
+
+                again = GoAgain();  //ask user if they would like to create another file
+
+            } while (again == true);
+
+        }
+
+        static public bool GoAgain()
+        {
+            Console.WriteLine("\nWould you like to create another file?");
+            Console.WriteLine("Y/N:");
+            string response = Console.ReadLine();
+
+            switch ( response.ToUpper() )   //Will still accept lowercase responses
             {
-                Console.WriteLine("Exception: " + e.Message);
+                case "Y":
+                    return true;
+                case "N":
+                    Console.WriteLine("Exiting program.");
+                    return false;
+                default:
+                    Console.WriteLine("That is not a valid response. Exiting program.");
+                    return false;
             }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
-            }
-
         }
     }
 }
